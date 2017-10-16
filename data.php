2da83373daad8c86b10e76591ea8fda895a775fc
Sebastian Palm <sebastian.palm91@gmail.com>
2017-10-16 22:25:25 +0200

    added some color to .likes and added like logo

--- data.php
+++ data.php
@@ -16,7 +16,7 @@ $contents = [
                            scissors decapitates lizard, lizard eats paper, paper disproves Spock, Spock vaporizes rock, and as it always has, rock crushes scissors."',
                'authorname' => '— Sheldon Cooper, Bing Bang Theory',
                'pubdate' => '2013-06-04',
-               'likes' => 4
+               'likes' => 19
              ],
 
   'Barney2' => ['name' => 'Barney Stinson',
@@ -29,15 +29,15 @@ $contents = [
                           That\'s why we wait three days to call a woman, because that\'s how long Jesus wants us to wait.... True story.',
                'authorname' => '- Barney Stinson, HIMYM',
                'pubdate' => '2016-03-10',
-               'likes' => 24
+               'likes' => 112
              ],
 
   'Barney3' => ['name' => 'Sheldon Cooper',
                'text' => 'I know the real reason you never made progress with that idea. You thought of it September 22nd 2007.
                           Two days later, Penny moved in and so much blood rushed to your genitals, your brain became a ghost town.',
                'authorname' => '— Sheldon Cooper, Bing Bang Theory',
-               'pubdate' => '2015-10-07',
-               'likes' => 24
+               'pubdate' => '2014-01-07',
+               'likes' => 87
              ],
 
   'Barney5' => ['name' => 'Barney Stinson',
@@ -45,50 +45,50 @@ $contents = [
                          Corollary: "If there happens to be a hot chick driving the car next to the Bro, the Bro shall put his sunglasses down to get a better look.
                          If he\'s not wearing his sunglasses, he will first put them on, then pull down to get a better look.”',
                'authorname' => '- Barney Stinson',
-               'pubdate' => '2015-10-07',
-               'likes' => 24
+               'pubdate' => '2014-05-17',
+               'likes' => 224
              ],
 
   'Barney4' => ['name' => 'Sheldon Cooper',
               'text' => '“There\'s no denying that I have feelings for you that can\'t be explained in any other way.
                           I briefly considered that I had a brain parasite, but that seems even more far-fetched. The only conclusion was love."',
               'authorname' => '— Sheldon Cooper, Bing Bang Theory',
-              'pubdate' => '2015-10-07',
-              'likes' => 24
+              'pubdate' => '2015-07-04',
+              'likes' => 93
              ],
 
   'Barney6' => ['name' => 'Barney Stinson',
                'text' => '“Article 24: "When wearing a baseball cap,
                            a Bro may position the brim at either 12 or 6 o’clock. All other angles are reserved for rappers and the handicapped.” ',
                'authorname' => '- Barney Stinson',
-               'pubdate' => '2015-10-07',
-               'likes' => 24
+               'pubdate' => '2014-12-27',
+               'likes' => 36
              ],
   'Barney7' => ['name' => 'Sheldon Cooper',
                'text' => '“The correct animal for inter-species super soldier is the koala.
                            You would wind up with an army so cute, it couldn\'t be attacked.',
                'authorname' => '— Sheldon Cooper, Bing Bang Theory',
-               'pubdate' => '2015-10-07',
-               'likes' => 24
+               'pubdate' => '2013-11-09',
+               'likes' => 76
               ],
   'Barney8' => ['name' => 'Barney Stinson',
                'text' => '“ARTICLE 54 A Bro is required to go out with his Bros on St. Patty’s Day and other official Bro holidays,
                            including Halloween, New Year’s Eve, and Desperation Day (February 13).”',
                'authorname' => '- Barney Stinson',
-               'pubdate' => '2015-10-07',
-               'likes' => 24
+               'pubdate' => '2012-02-19',
+               'likes' => 47
               ],
    'Barney9' => ['name' => 'Sheldon Cooper',
                'text' => '“Oh gravity, thou art a heartless bitch."',
                'authorname' => '— Sheldon Cooper, Bing Bang Theory',
-               'pubdate' => '2015-10-07',
-               'likes' => 24
+               'pubdate' => '2011-06-14',
+               'likes' => 198
               ],
    'Barney10' => ['name' => 'Barney Stinson',
                'text' => '“A lie is just a really great story that someone ruined with the truth.”',
                'authorname' => '- Barney Stinson',
-               'pubdate' => '2015-10-07',
-               'likes' => 24
+               'pubdate' => '2010-09-21',
+               'likes' => 456
               ],
 ];
 
--- functions.php
+++ functions.php
@@ -9,16 +9,4 @@ declare(strict_types=1);
    }else {
       $_SESSION['views'] = 1;
    }
-
-
-//
-// function get_time_string(array $seconds)
-// {
-// $seconds = [];
-//
-// foreach ($seconds as $second) {
-//   array_push($seconds);
-// }
-//
-//   die var_dump (return date('H:i:s', strtotime("2000-01-01 + $seconds SECONDS")));
-// }
+?>
