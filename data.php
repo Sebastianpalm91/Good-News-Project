7b2b006479885759bda630e371d3c9fa02d7bb1e
Sebastian Palm <sebastian.palm91@gmail.com>
2017-10-16 15:00:29 +0200

    Added html css, PHP data

---
+++ data.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+$contents = [
+  'Barney' => ['name' => 'Barney Stinson',
+               'text' => '“When I get sad, I stop being sad and be awesome instead.”',
+               'authorname' => '<br>    - Barney Stinson',
+               'pubdate' => '2014-08-20',
+               'likes' => 24,
+              ],
+
+  'Barney' => ['name' => 'Barney Stinson',
+               'text' => '“A lie is just a great story that someone ruined with the truth.”',
+               'authorname' => '<br>    - Barney Stinson',
+               'pubdate' => '2013-06-04',
+               'likes' => 4,
+              ],
+
+  'Barney' => ['name' => 'Barney Stinson',
+               'text' => 'Jesus waited three days to come back to life. It was perfect! If he had only waited one day, a lot of people wouldn\'t have even heard he died.
+                          They\'d be all, "Hey Jesus, what up?" and Jesus would probably be like, "What up? I died yesterday!" and they\'d be all, "Uh, you look pretty alive to me, dude..."
+                          and then Jesus would have to explain how he was resurrected, and how it was a miracle, and the dude\'d be like "Uhh okay, whatever you say, bro..." And he\'s not gonna come back on a Saturday. Everybody\'s busy,
+                          doing chores, workin\' the loom, trimmin\' the beard, NO. He waited the perfect number of days, three.
+                          Plus it\'s Sunday, so everyone\'s in church already, and they\'re all in there like "Oh no, Jesus is dead",
+                          and then BAM! He bursts in the back door, runnin\' up the aisle, everyone\'s totally psyched, and FYI, that\'s when he invented the high five.
+                          That\'s why we wait three days to call a woman, because that\'s how long Jesus wants us to wait.... True story.',
+               'authorname' => '<br>    - Barney Stinson',
+               'pubdate' => '2014-08-20',
+               'likes' => 24,
+              ],
+
+  'Barney' => ['name' => 'Barney Stinson<br>',
+               'text' => '“Article 100: "When pulling up to a stoplight, a Bro lowers his window so that all might enjoy his music selection."
+                          Corollary: "If there happens to be a hot chick driving the car next to the Bro, the Bro shall put his sunglasses down to get a better look.
+                          If he\'s not wearing his sunglasses, he will first put them on, then pull down to get a better look.”',
+               'authorname' => '<br>    - Barney Stinson',
+               'pubdate' => '<br>2014-08-20<br>',
+               'likes' => 24,
+              ],
+
+
+]
+
+
+
+ ?>
--- index.php
+++ index.php
@@ -1,13 +1,32 @@
+<?php
+require __DIR__.'/data.php';
+require __DIR__.'/functions.php';
+
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/master.css">
+    <link rel="stylesheet" href="main.css">
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous"> -->
     <title>Good-News</title>
   </head>
   <body>
-    
-  </body>
+    <?php  ?>
+    <img class="header-barney" src="https://media.giphy.com/media/QN6NnhbgfOpoI/giphy.gif"</a>
+        <div class="container">
+          <div class="contain">
+            <?php foreach ($contents as $id => $content)?>
+              <div class="name"><?php echo $content['name'] ?></div>
+              <div class="text"><?php echo $content['text'] ?></div>
+              <div class="authorname"><?php echo $content['authorname'] ?></div>
+              <div class="pubdate"><?php echo $content['pubdate'] ?></div>
+              <div class="likes"><?php echo $content['likes'] ?></div>
+            <?php endforeach; ?>
+          </div>
+        </div>
+    </body>
 </html>
